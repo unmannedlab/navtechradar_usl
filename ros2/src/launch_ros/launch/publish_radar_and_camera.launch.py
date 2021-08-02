96e18b9f9cb3d065f7168c2e7c7bed8831c5ef89
Ben Guy <ben.guy@navtechradar.com>
2021-08-02 10:00:04 +0100

    Added launch file to start radar and camera publishing with params

--- ros2/src/launch_ros/launch/publish_radar_and_camera.launch.py
+++ ros2/src/launch_ros/launch/publish_radar_and_camera.launch.py
@@ -5,10 +5,12 @@ def generate_launch_description():
     launchDescription = LaunchDescription()
     colossus_publisher_node = Node(
         package="nav_ros",
+        parameters=["./src/nav_ros/config/colossus_publisher.yaml"],
         executable="colossus_publisher",
     )
     camera_publisher_node = Node(
         package="camera_ros",
+        parameters=["./src/camera_ros/config/camera_publisher.yaml"],
         executable="camera_publisher",
     )
     launchDescription.add_action(colossus_publisher_node)
